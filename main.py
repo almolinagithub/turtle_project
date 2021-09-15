ae2af6b58bf813982c5ea3a671fcc3525f0683c6
Noodles Florence <alessandromolinari2000@hotmail.com>
2021-09-15 19:56:49 +0200

    first commit

--- main.py
+++ main.py
@@ -1,24 +1,50 @@
 import random
+import colorgram
 from turtle import Turtle
 from turtle import Screen
 
+
+
 turtle = Turtle()
+turtle.speed(10000)
 screen = Screen()
+screen.colormode(255)
+screen.screensize(500, 500)
+
+colors = colorgram.extract("/Users/admin/PycharmProjects/turtle_project/images.jpg", 20)
+
+
+def print_colored_dot(color):
+    turtle.pendown()
+    rgb = color.rgb
+    turtle.color(rgb)
+    turtle.begin_fill()
+    turtle.circle(10)
+    turtle.end_fill()
+    turtle.penup()
+    turtle.forward(30)
 
 
 
-def random_color():
-    r = random.uniform(0, 1)
-    g = random.uniform(0, 1)
-    b = random.uniform(0, 1)
-    color = (r, g, b)
-    return color
+
+turtle.penup()
+
+y = -300
+x = -300
+
+for _ in range(len(colors)):
+    turtle.home()
+    turtle.sety(y)
+    turtle.setx(x)
+    for color in colors:
+        print_colored_dot(color)
+
+    y += 30
 
 
-for i in range(360):
-    turtle.pencolor(random_color())
-    turtle.speed("fastest")
-    turtle.circle(100)
-    turtle.right(3)
+
+
+
+
 
 screen.exitonclick()
